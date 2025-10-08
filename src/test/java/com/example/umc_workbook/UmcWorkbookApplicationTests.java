4f46456334e41e1d36dfb22dd3252f13f512af15
ryun <ryun0038@gmail.com>
2025-10-08 22:41:53 +0900

    no message

---
+++ src/test/java/com/example/umc_workbook/UmcWorkbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.umc_workbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmcWorkbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
