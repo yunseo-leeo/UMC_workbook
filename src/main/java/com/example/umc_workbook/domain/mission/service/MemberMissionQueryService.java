5dfe2ea39ebb53c8b63bb38ddaf6fba1477f1da7
ryun <ryun0038@gmail.com>
2025-11-26 11:14:46 +0900

    [feat] 진행 중인 미션 목록 보기 api 추가

--- src/main/java/com/example/umc_workbook/domain/mission/service/MemberMissionQueryService.java
+++ src/main/java/com/example/umc_workbook/domain/mission/service/MemberMissionQueryService.java
@@ -1,15 +1,21 @@
 package com.example.umc_workbook.domain.mission.service;
 
-import com.example.umc_workbook.domain.mission.dto.MemberMissionRequestDto;
-import com.example.umc_workbook.domain.mission.dto.MemberMissionResponseDto;
+import com.example.umc_workbook.domain.member.exception.MemberErrorCode;
+import com.example.umc_workbook.domain.member.exception.MemberException;
+import com.example.umc_workbook.domain.member.repository.MemberRepository;
+import com.example.umc_workbook.domain.mission.converter.MemberMissionConverter;
+import com.example.umc_workbook.domain.mission.dto.MissionResDto;
+import com.example.umc_workbook.domain.mission.entity.MemberMission;
+import com.example.umc_workbook.domain.mission.enums.MissionStatus;
+import com.example.umc_workbook.domain.mission.exception.MemberMissionErrorCode;
 import com.example.umc_workbook.domain.mission.exception.MemberMissionException;
 import com.example.umc_workbook.domain.mission.repository.MemberMissionRepository;
-import com.example.umc_workbook.global.apiPayload.code.GeneralErrorCode;
+import com.example.umc_workbook.global.config.PaginationConstants;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,19 +23,20 @@ import java.util.List;
 public class MemberMissionQueryService {
 
     private final MemberMissionRepository memberMissionRepository;
+    private final MemberRepository memberRepository;
 
-    public List<MemberMissionResponseDto> findMemberMissions(Long memberId, MemberMissionRequestDto req) {
+    public MissionResDto.MemberMissionListDto findMemberMissions(Long memberId, Integer page) {
 
-        if (memberId == null) {
-            throw new MemberMissionException(GeneralErrorCode.UNAUTHORIZED);
+        if(!memberRepository.existsById(memberId)) {
+            throw new MemberException(MemberErrorCode.MEMBER_NOT_FOUND);
         }
 
-        List<MemberMissionResponseDto> missions = memberMissionRepository.findMemberMissions(memberId, req, req.toPageable());
+        PageRequest pageRequest = PageRequest.of(page-1, PaginationConstants.DEFAULT_PAGE_SIZE);
+        Page<MemberMission> result = memberMissionRepository.findByMemberIdAndMissionStatus(memberId, MissionStatus.IN_PROGRESS, pageRequest);
 
-        if (missions.isEmpty()) {
-            throw new MemberMissionException(GeneralErrorCode.NOT_FOUND);
+        if (result.isEmpty()) {
+            throw new MemberMissionException(MemberMissionErrorCode.MEMBER_MISSION_NOT_FOUND);
         }
-
-        return missions;
+        return MemberMissionConverter.toMemberMissionListDto(result);
     }
 }
